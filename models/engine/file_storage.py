fbce4c19f952e0651337d64425749b33fd33aa41
wac <wabaham9@gmail.com>
2023-02-15 10:06:24 +0000

    v3/2 cor2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except as e:
             pass
 
     def delete(self, obj=None):
@@ -68,7 +68,7 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-    
+
     def get(self, cls, id):
         """ get an object based on class name and id """
         if cls is not None or id is not None:
