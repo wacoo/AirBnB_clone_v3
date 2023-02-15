1b75ebe47379a611259e0b6b9c30ee114a49f1a8
wac <wabaham9@gmail.com>
2023-02-15 10:16:06 +0000

    v3 cor

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -80,7 +80,7 @@ class DBStorage:
         if cls is not None and id is not None:
             try:
                 return self.__session.query(classes[cls]).get(id)
-            except as e:
+            except:
                 return None
         return None
 
@@ -90,7 +90,8 @@ class DBStorage:
         if cls is not None:
             try:
                 return len(self.all(classes[cls]))
-            except as e:
+            except:
                 return None
         else:
             return len(self.all())
+
