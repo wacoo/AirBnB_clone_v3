3fe3dab69521bd1675b80f850bd3733c0023e88b
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-02-16 18:29:23 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-""" holds class Review"""
+"""holds class Review"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -8,7 +8,7 @@ from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """Representation of Review """
+    """Representation of Review"""
     if models.storage_t == 'db':
         __tablename__ = 'reviews'
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
