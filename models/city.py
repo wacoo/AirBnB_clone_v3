5ad54bdb9c2c2977165ebe1079424d5e460d50fe
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-02-16 18:33:15 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-""" holds class City"""
+"""holds class City"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """Representation of city """
+    """Representation of city"""
     if models.storage_t == "db":
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
